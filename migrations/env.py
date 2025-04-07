4b1d1a2f6c0b43d8b9142f861f60a10857a88a4e
Abhishek-Rajopadhye <abhishek.rajopadhye21@gmail.com>
2025-04-07 10:17:23 +0530

    Updated alembic migrations cofig

--- migrations/env.py
+++ migrations/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from app.db.session import Base  # Import Base from session.py
+from app.db.db import Base  # Import Base from session.py
 from app.db.oauth_model import OAuthUser  # Ensure models are imported
 
 # this is the Alembic Config object, which provides
