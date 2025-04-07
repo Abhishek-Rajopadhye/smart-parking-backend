7770c2b3b11b9b4388807e543fae813e71607a5f
Abhishek-Rajopadhye <abhishek.rajopadhye21@gmail.com>
2025-04-07 10:18:26 +0530

    Updates database handling and creation to alembic

--- app/db/session.py
+++ app/db/session.py
@@ -20,7 +20,7 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} i
 SessionLocal = sessionmaker(autoflush=False, bind=engine)
 
 # Create all tables
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 # Dependency to get a session instance
 def get_db():
