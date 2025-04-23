d876b1b241f1779edca8844070536a55b7fb3f5b
Arjun <arjunghule1113@gmail.com>
2025-04-23 12:46:47 +0530

    .

---
+++ app/locustfile.py
@@ -0,0 +1,21 @@
+from locust import HttpUser, task, between
+
+class BookingUser(HttpUser):
+    # Define the wait time between each task
+    wait_time = between(1, 3)  # Wait between 1 and 3 seconds between each request
+    
+    @task
+    def book_spot(self):
+        # Define the POST request to your /bookings/book-spot endpoint
+        payload = {
+            "user_id": "111919577987638512190",
+            "spot_id": 42,
+            "total_slots": 4,
+            "start_date_time": "2023-10-01T10:00:00",
+            "end_date_time": "2023-10-01T12:00:00",
+            "total_amount": 20,
+            "receipt": "mock_receipt"
+        }
+        
+        # Perform the POST request
+        self.client.post("/bookings/book-spot/", json=payload)
